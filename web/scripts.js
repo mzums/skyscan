b816f1ef0aae1b8ce3a2a1d4c082e9ca7cd8ad4b
mzums <annsie035@gmail.com>
2023-09-16 15:15:07 +0200

    getting data

--- web/scripts.js
+++ web/scripts.js
@@ -1,6 +1,21 @@
+//document.getElementById('city').value = ''
 document.getElementById("city").addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
-        // Tutaj możesz umieścić kod, który ma się wykonać po wciśnięciu Enter
-        alert("Wcisnąłeś Enter po wpisaniu tekstu: " + this.value);
+        fetch('http://api.openweathermap.org/data/2.5/weather?q='+this.value+'&units=metric&appid=e1e260bb69ce72bf8562cebe1feb4ee3')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error, status: " + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data['weather'][0]['description']);
+            var output = document.getElementById("output");
+            output.innerHTML = this.value + "<br>" + data['weather'][0]['description'];
+        })
+        .catch(err => {
+            //console.error(err);
+            alert("Error downloading weather data!");
+        });
     }
 });
